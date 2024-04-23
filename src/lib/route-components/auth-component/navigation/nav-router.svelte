93bbecce21a77bb87bc4de5a282970d119411661
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-23 18:22:18 +0800

    add native width

--- src/lib/route-components/auth-component/navigation/nav-router.svelte
+++ src/lib/route-components/auth-component/navigation/nav-router.svelte
@@ -30,7 +30,11 @@
 	];
 
 	let showMobileSlider = false;
+
+	let nativeWidth = 0;
 </script>
+
+<svelte:window bind:innerWidth={nativeWidth} />
 
 <nav class="flex items-center border-b-[1px] p-[10px]">
 	<button on:click={() => (showMobileSlider = true)}>
@@ -38,45 +42,47 @@
 	</button>
 </nav>
 
-<Sheet.Root bind:open={showMobileSlider}>
-	<Sheet.Content side="left">
-		<Sheet.Header class="mt-[30px] flex flex-col gap-[20px]">
-			<div class="mx-auto flex flex-col items-center gap-[10px]">
-				<Avatar.Root class="h-[100px] w-[100px]">
-					<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
-					<Avatar.Fallback>CN</Avatar.Fallback>
-				</Avatar.Root>
+{#if nativeWidth < 640}
+	<Sheet.Root bind:open={showMobileSlider}>
+		<Sheet.Content side="left">
+			<Sheet.Header class="mt-[30px] flex flex-col gap-[20px]">
+				<div class="mx-auto flex flex-col items-center gap-[10px]">
+					<Avatar.Root class="h-[100px] w-[100px]">
+						<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+						<Avatar.Fallback>CN</Avatar.Fallback>
+					</Avatar.Root>
 
-				<Button>Edit Profile</Button>
-			</div>
+					<Button>Edit Profile</Button>
+				</div>
 
-			<div class="">
-				<Sheet.Title class="text-left">Eviota, Mike John Baguinaon</Sheet.Title>
-				<Sheet.Description class="max-h-[100px] overflow-auto text-left">
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, totam. Ut,
-					maxime aperiam magnam sapiente cum pariatur veritatis, dolorem culpa nemo eos et voluptas
-					nesciunt, maiores fuga adipisci optio autem quis sed? Sit atque natus suscipit quod
-					quaerat saepe iure voluptates commodi dicta sint dolorem sequi at consectetur maiores ea,
-					officia incidunt delectus necessitatibus voluptate harum quae tempora consequatur
-					quisquam? Sequi ipsa quae ipsum voluptatem ullam rem officia sunt qui, odio perspiciatis
-					velit quo dolor amet natus, tenetur provident modi impedit laudantium veritatis.
-					Blanditiis facilis in, sunt minima commodi quia tempora consectetur aliquam laudantium
-					incidunt optio perferendis illo aperiam. Animi!
-				</Sheet.Description>
-			</div>
+				<div class="">
+					<Sheet.Title class="text-left">Eviota, Mike John Baguinaon</Sheet.Title>
+					<Sheet.Description class="max-h-[100px] overflow-auto text-left">
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, totam. Ut,
+						maxime aperiam magnam sapiente cum pariatur veritatis, dolorem culpa nemo eos et
+						voluptas nesciunt, maiores fuga adipisci optio autem quis sed? Sit atque natus suscipit
+						quod quaerat saepe iure voluptates commodi dicta sint dolorem sequi at consectetur
+						maiores ea, officia incidunt delectus necessitatibus voluptate harum quae tempora
+						consequatur quisquam? Sequi ipsa quae ipsum voluptatem ullam rem officia sunt qui, odio
+						perspiciatis velit quo dolor amet natus, tenetur provident modi impedit laudantium
+						veritatis. Blanditiis facilis in, sunt minima commodi quia tempora consectetur aliquam
+						laudantium incidunt optio perferendis illo aperiam. Animi!
+					</Sheet.Description>
+				</div>
 
-			<div class="mt-[20px] flex flex-col gap-[20px]">
-				{#each navigation as route}
-					<Sheet.Title class="text-left">
-						<a
-							on:click={() => ($authState.activeURL = route.url)}
-							href={route.url}
-							class={$authState.activeURL === route.url ? 'text-muted-foreground underline' : ''}
-							>{route.title}</a
-						>
-					</Sheet.Title>
-				{/each}
-			</div>
-		</Sheet.Header>
-	</Sheet.Content>
-</Sheet.Root>
+				<div class="mt-[20px] flex flex-col gap-[20px]">
+					{#each navigation as route}
+						<Sheet.Title class="text-left">
+							<a
+								on:click={() => ($authState.activeURL = route.url)}
+								href={route.url}
+								class={$authState.activeURL === route.url ? 'text-muted-foreground underline' : ''}
+								>{route.title}</a
+							>
+						</Sheet.Title>
+					{/each}
+				</div>
+			</Sheet.Header>
+		</Sheet.Content>
+	</Sheet.Root>
+{/if}

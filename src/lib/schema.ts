97d1468d1f16f17550775f0d3e916c596b8886bc
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-24 01:38:14 +0800

    add node mailer

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -6,6 +6,7 @@ export const loginSchema = z.object({
 });
 
 export const registerSchema = z.object({
+    passwordStrength: z.string(),
     firstName: z.string().min(1, { message: "Must enter a valid first name." }).max(10, { message: "Maximum char for first name is 10." }),
     lastName: z.string().min(1, { message: "Must enter a valid last name." }).max(10, { message: "Maximum char for last name is 10." }),
     email: z.string().email().max(20, { message: "Maximum char for email is 10." }),

0142543887bcbb487c15edbf43c72a37e74f2afc
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-23 16:22:00 +0800

    add login

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import Login from '$lib/route-components/static-component/login.svelte';
 </script>
 
-<div class="">Hello wierd</div>
+<div class="flex min-h-screen flex-col items-center justify-center">
+	<Login />
+</div>

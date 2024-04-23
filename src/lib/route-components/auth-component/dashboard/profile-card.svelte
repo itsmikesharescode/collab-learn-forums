4a154ac5c836dbad9f4e62a535d32370a31f8b95
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-24 01:53:45 +0800

    update dashboard profile card

--- src/lib/route-components/auth-component/dashboard/profile-card.svelte
+++ src/lib/route-components/auth-component/dashboard/profile-card.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
+	import { getUserState } from '$lib';
 	import * as Avatar from '$lib/components/ui/avatar/index';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import * as Card from '$lib/components/ui/card';
+
+	const userState = getUserState();
 </script>
 
 <Card.Root class="relative flex flex-wrap p-[20px]">
 	<Avatar.Root class="h-[100px] w-[100px]">
-		<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+		<Avatar.Image src={$userState?.user_photo_link} alt="@shadcn" />
 		<Avatar.Fallback>CN</Avatar.Fallback>
 	</Avatar.Root>
 	<Card.Header>
 		<Button class="absolute right-0 top-0 m-[20px]">Edit Profile</Button>
-		<Card.Title>Eviota, Mike John Baguinaon</Card.Title>
-		<Card.Description
-			>The king, seeing how much happier his subjects were, realized the error of his ways and
-			repealed the joke tax.</Card.Description
-		>
+		<Card.Title>{$userState?.user_fullname}</Card.Title>
+		<Card.Description>{$userState?.user_bio}</Card.Description>
 	</Card.Header>
 </Card.Root>

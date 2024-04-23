88a03085fb0d267bb8f95dedb24c0eca6418f9b7
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-23 16:28:00 +0800

    add logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,23 @@
 <script lang="ts">
+	import { getStaticState, setStaticState } from '$lib';
+	import ForgotPassword from '$lib/route-components/static-component/forgot-password.svelte';
 	import Login from '$lib/route-components/static-component/login.svelte';
+	import Register from '$lib/route-components/static-component/register.svelte';
+
+	setStaticState({
+		register: false,
+		forgotPass: false
+	});
+
+	const staticState = getStaticState();
 </script>
 
 <div class="flex min-h-screen flex-col items-center justify-center">
-	<Login />
+	{#if $staticState.register}
+		<Register />
+	{:else if $staticState.forgotPass}
+		<ForgotPassword />
+	{:else}
+		<Login />
+	{/if}
 </div>

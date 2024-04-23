cb29c7d35362740011edf5ce13d06ae7f76b16c1
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-23 22:28:01 +0800

    update types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -13,3 +13,19 @@ export interface StaticStates {
 export interface AuthState {
     activeURL: string
 }
+
+//user_list_tb
+export type UserReference = {
+    id: number
+    created_at: string
+    user_id: string
+    user_email: string
+    user_fullname: string
+    user_photo_link: string
+    user_bio: string
+    user_address: string
+    user_barangay: string
+    user_city: string
+    user_religion: string
+    user_contact: string
+}

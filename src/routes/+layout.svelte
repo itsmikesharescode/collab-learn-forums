a74cbfc87537a7e8897000127ef9c79538c6c9be
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-23 17:00:56 +0800

    add darkmode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,11 @@
-<script>import "../app.pcss";</script><slot></slot>
+<script lang="ts">
+	import DarkMode from '$lib/route-components/static-component/dark-mode.svelte';
+	import '../app.pcss';
+	import { ModeWatcher } from 'mode-watcher';
+</script>
+
+<ModeWatcher />
+<div class="fixed bottom-0 m-[10px]">
+	<DarkMode />
+</div>
+<slot />

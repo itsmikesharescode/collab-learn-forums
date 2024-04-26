756d2cee167e6cffd09e6dd59a175f8b5de46758
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-27 02:15:02 +0800

    add wall post fetcher

--- src/routes/(authenticated)/guilds/[guild_content]/+page.svelte
+++ src/routes/(authenticated)/guilds/[guild_content]/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import type { LayoutServerData } from '../../$types';
-	import { getAuthState } from '$lib';
+	import { getAuthState, supabase } from '$lib';
 	import { goto } from '$app/navigation';
 	import GuildContent from '$lib/route-components/auth-component/guilds/guild-content.svelte';
 
@@ -11,16 +11,31 @@
 
 	let hiddenContent = true;
 
+	const { guildObj } = $authState.guilds;
 	const tempStore = data.createdGuilds.data?.map((guild) => guild.storage_id);
+
+	const getWallPost = async () => {
+		if ($supabase) {
+			const { data, error } = await $supabase
+				?.from('guild_wall_tb_post_new')
+				.select('*')
+				.order('created_at', { ascending: true })
+				.eq('guild_id', guildObj?.id);
+		}
+	};
 
 	onMount(async () => {
 		if (tempStore) {
 			const includes = tempStore.includes($authState.guilds.guildObj?.storage_id ?? '');
 
 			if (includes) return (hiddenContent = false);
-			return await goto('/guilds?=not-allowed');
+			return await goto('/dashboard?=that-route-is-secure');
 		}
 	});
 </script>
 
-<GuildContent />
+{#if hiddenContent}
+	<p>Checking Key</p>
+{:else}
+	<GuildContent />
+{/if}
